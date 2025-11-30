2ed4fd3a93d39aff5d52d5e3239a93c610640e17
MohamedMS-1 <mselmii.mohamed@gmail.com>
2025-11-30 15:11:27 +0100

    Updates

---
+++ precache-manifest.a6a2cc6ee1f6d2d8ca2d2429b58e8be4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980dbd4a2b84e14adf69eae404e3763c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aaac50f2f91efc96f621",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d0979f1c5d1e6d009fb4",
+    "url": "/portfolio/static/css/main.8d108722.chunk.css"
+  },
+  {
+    "revision": "aaac50f2f91efc96f621",
+    "url": "/portfolio/static/js/2.06559cb8.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.06559cb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0979f1c5d1e6d009fb4",
+    "url": "/portfolio/static/js/main.561205c5.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
